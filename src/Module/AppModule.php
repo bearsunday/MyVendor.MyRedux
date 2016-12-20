fa2223005e39c56a28f7e8a795fe2c3f29fbc54e
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-12-20 19:06:46 +0900

    install ReduxModule

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -3,6 +3,7 @@
 namespace MyVendor\MyRedux\Module;
 
 use BEAR\Package\PackageModule;
+use BEAR\ReactJsModule\ReduxModule;
 use Ray\Di\AbstractModule;
 use josegonzalez\Dotenv\Loader as Dotenv;
 
@@ -18,5 +19,8 @@ class AppModule extends AbstractModule
             'toEnv' => true
         ]);
         $this->install(new PackageModule);
+        //configure()に追加
+        $distDir = dirname(__DIR__, 2) . '/var/www/dist';
+        $this->install(new ReduxModule($distDir, 'ssr_hello'));
     }
 }
